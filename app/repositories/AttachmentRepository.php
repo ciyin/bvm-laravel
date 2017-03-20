f6b431f2947fa0c99991cc47bf735dc3429fee02
liudanxia <liudanxia@onebest.cn>
2017-03-20 17:38:02 +0800

    simplify sentence

--- app/repositories/AttachmentRepository.php
+++ app/repositories/AttachmentRepository.php
@@ -16,13 +16,13 @@ class AttachmentRepository{
     protected $attachment;
 
 //    实例化attachment对象
-    public function newAttachment($request,$original_name,$random_name,$url){
+    public function newAttachment($request,$original_name,$random_name,$saved_at){
         $attachment=new Attachment();
         $attachment->original_name=$original_name;
         $attachment->random_name=$random_name;
         $attachment->status='1';
         $attachment->note=$request->note;
-        $attachment->saved_at=$url;
+        $attachment->saved_at=$saved_at;
         $attachment->user_id=Auth::id();
         return $attachment;
     }
@@ -30,30 +30,25 @@ class AttachmentRepository{
 //       保存附件
     public function storeAttachment($request){
         $file=$request->file('attachment');
-//       得到附件的名称
-        $original_name=$file->getClientOriginalName();
-//       得到附件的后缀名
-        $ext=$file->getClientOriginalExtension();
-//       得到附件的绝对存储路径
-        $realPath=$file->getRealPath();
-//       重新命名附件名称再保存，防止原附件名称有不可识别的字符，到时取不出来
-        $random_name=str_random(5).'.'.$ext;
-//       将附件保存到七牛
-        $disk=\Storage::disk('qiniu');
-        $res=$disk->put($random_name,file_get_contents($realPath));
+        $original_name=$file->getClientOriginalName(); //得到附件的名称
+        $ext=$file->getClientOriginalExtension(); //得到附件的后缀名
+        $realPath=$file->getRealPath(); //得到附件的绝对存储路径
+        $random_name=str_random(5).'.'.$ext; //重新命名附件名称再保存，防止原附件名称有不可识别的字符，到时取不出来
+        $disk=\Storage::disk('qiniu'); //选择存储的磁盘
+        $res=$disk->put($random_name,file_get_contents($realPath)); //将附件保存到七牛
 //       如果成功保存，则返回附件的保存地址，并将附件信息存入库；
         if ($res){
-            $url=$disk->getDriver()->downloadUrl($random_name);
+            $saved_at=$disk->getDriver()->downloadUrl($random_name);
             $versions=$request->related_version;
             if ($versions){
                 $count=count($versions);
                 for ($i=0;$i<$count;$i++){
-                    $this->attachment=$this->newAttachment($request,$original_name,$random_name,$url);
+                    $this->attachment=$this->newAttachment($request,$original_name,$random_name,$saved_at);
                     $version=Version::find($versions[$i]);
                     $version->attachments()->save($this->attachment);
                 }
             }else{
-                $this->attachment=$this->newAttachment($request,$original_name,$random_name,$url);
+                $this->attachment=$this->newAttachment($request,$original_name,$random_name,$saved_at);
                 $book=Book::find($request->book_id);
                 $book->attachments()->save($this->attachment);
             }
--- resources/views/nav/admin.blade.php
+++ resources/views/nav/admin.blade.php
@@ -3,6 +3,8 @@
     <li><a href="{{route('user.index')}}">用户管理</a></li>
     <li><a href="{{route('examtype.index')}}">考试类型</a></li>
     <li><a href="{{route('booktype.index')}}">教材分类</a></li>
+    <li><a href="">使用分类</a></li>
     <li><a href="{{route('book.index')}}">教材列表</a></li>
     <li><a href="">操作记录</a></li>
+    <li><a href="">教材统计</a></li>
 </ul>
